6753d0aca911c7a4f585c087ef9c025eaab1cfa4
PabloCou <pablocourou@gmail.com>
2025-03-11 11:26:36 +0100

    v0.6.4

--- src/routes/offer.routes.ts
+++ src/routes/offer.routes.ts
@@ -9,28 +9,20 @@ const router = Router();
 
 // API REST FULL
 
-// Ruta para crear una oferta
 router.post('/game-offers/new', isAuthenticate, isAdmin, offerValidation, ValidationMiddleware, OfferController.create);
 
-// Ruta para obtener todas las ofertas
 router.get('/', isAuthenticate, OfferController.getAll);
 
-// Ruta para obtener una oferta por ID
 router.get('/:id', isAuthenticate, OfferController.getById);
 
-// Ruta para eliminar una oferta
 router.delete('/:id', isAuthenticate, isAdmin, OfferController.delete);
 
-// Ruta para actualizar una oferta
 router.put('/:id', isAuthenticate, isAdmin, offerValidation, ValidationMiddleware, OfferController.update);
 
-// Ruta para calificar una oferta
 router.post('/:id/rate/', isAuthenticate, rateValidation, OfferController.rate);
 
-// Ruta para obtener la calificación total de una oferta
 router.get('/:id/rate/', isAuthenticate, OfferController.getRate);
 
-// Ruta para obtener la calificación específica de un usuario para una oferta
 router.get('/:id/myRate/', isAuthenticate, OfferController.getMyRate);
 
 export default router;
