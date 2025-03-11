4e07bd3ee58b9ee86561b59efa6921d8e7367d1b
PabloCou <pablocourou@gmail.com>
2025-03-11 09:32:57 +0100

    v0.6.1

--- prisma/migrations/20250311082755_update/migration.sql
+++ prisma/migrations/20250311082755_update/migration.sql
@@ -5,30 +5,11 @@ CREATE TABLE "User" (
     "surname" TEXT,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "role" TEXT,
-    "course" TEXT,
+    "role" TEXT DEFAULT 'USER',
     "active" BOOLEAN NOT NULL DEFAULT true,
     "accepNotifications" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
-);
-
--- CreateTable
-CREATE TABLE "Offer" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "description" TEXT,
-    "active" BOOLEAN NOT NULL DEFAULT true,
-    "contactEmail" TEXT,
-    "location" TEXT,
-    "published" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "expired" DATETIME NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "idCategory" INTEGER,
-    "idUserCreator" INTEGER NOT NULL,
-    CONSTRAINT "Offer_idCategory_fkey" FOREIGN KEY ("idCategory") REFERENCES "Category" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
-    CONSTRAINT "Offer_idUserCreator_fkey" FOREIGN KEY ("idUserCreator") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
@@ -49,7 +30,23 @@ CREATE TABLE "Rate" (
 
     PRIMARY KEY ("idUser", "idOffer"),
     CONSTRAINT "Rate_idUser_fkey" FOREIGN KEY ("idUser") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "Rate_idOffer_fkey" FOREIGN KEY ("idOffer") REFERENCES "Offer" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "Rate_idOffer_fkey" FOREIGN KEY ("idOffer") REFERENCES "GameOffer" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "GameOffer" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "releaseDate" DATETIME NOT NULL,
+    "price" REAL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "idCategory" INTEGER,
+    "idUserCreator" INTEGER NOT NULL,
+    CONSTRAINT "GameOffer_idCategory_fkey" FOREIGN KEY ("idCategory") REFERENCES "Category" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "GameOffer_idUserCreator_fkey" FOREIGN KEY ("idUserCreator") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
